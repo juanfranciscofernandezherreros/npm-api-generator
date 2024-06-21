f9bda551055a05e7d9fd118a35ac33045b1efe09
ATMIRA\juan.fernandez <juan.fernandez@atmira.com>
2024-06-21 09:13:18 +0200

    initial feature

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # mi-repositorio1
 
-Este es el README para mi-repositorio1.
+npm run setup:repo -- C:\Users\xxxxx\Desktop\config.json
+
+{
+  "projectName": "usuarios-q",
+  "username": "xxxx",
+  "newBranchName": "feature/XXXXX",
+  "targetDirectory": "usuarios-q",
+  "packageName": "com.example.usuarios",
+  "appClassName": "UsuariosApplication",
+  "entityName": "Usuarios",
+  "repositoryName": "UsuariosRepository",
+  "serviceName": "UsuariosService",
+  "controllerName": "UsuariosController",
+  "exceptionName": "ResourceNotFoundException",
+  "exceptionNameBadRequest": "BadRequestException",
+  "handlerName": "GlobalExceptionHandler",
+  "dtoName": "UsuariosDto",
+  "mapperName": "UsuariosMapper",
+  "entityFields": [
+    {"type": "String", "name": "apellidos", "nameEntity":"apellidos","isPrimaryKey": "N", "isNotNull": "N", "columnName": "APELLIDOS"},
+    {"type": "String", "name": "codigoPrueba", "nameEntity":"codigoPrueba","isPrimaryKey": "Y", "isNotNull": "N", "columnName": "CODIGO_PRUEBA"},
+    {"type": "String", "name": "codPerfil", "nameEntity":"codPerfil","isPrimaryKey": "Y", "isNotNull": "N", "columnName": "CODIGO_PERFIL"}
+  ],
+
+  "tableName": "USUARIOS",
+  "urlName": "usuarios",
+  "findByKeys": "find",
+  "search": "search",
+  "databaseConfig": {
+    "username": "sa",
+    "password": "",
+    "host": "jdbc:h2:mem:testdb"
+  }
+}
--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 const fs = require('fs');
 const { exec } = require('child_process');
-const setupRepo = require('./setupRepo');
+const path = require('path');
+
+// Verifica si se proporcionó la ruta del archivo JSON como argumento
+if (process.argv.length < 3) {
+    console.error('Uso: node index.js <ruta_al_archivo_json>');
+    process.exit(1);
+}
+
+// Obtener la ruta absoluta del archivo JSON desde los argumentos
+const jsonFilePath = path.resolve(process.argv[2]);
+
+// Función para validar el esquema del JSON
+const validateSchema = (config) => {
+    const requiredFields = [
+        "projectName", "username", "newBranchName", "targetDirectory", "packageName", 
+        "appClassName", "entityName", "repositoryName", "serviceName", "controllerName", 
+        "exceptionName", "exceptionNameBadRequest", "handlerName", "dtoName", "mapperName", 
+        "entityFields", "tableName", "urlName", "findByKeys", "search", "databaseConfig"
+    ];
+
+    const missingFields = requiredFields.filter(field => !config.hasOwnProperty(field));
+
+    if (missingFields.length > 0) {
+        console.error(`Faltan los siguientes campos requeridos en el archivo JSON: ${missingFields.join(', ')}`);
+        return false;
+    }
+
+    if (!Array.isArray(config.entityFields)) {
+        console.error('El campo "entityFields" debe ser un arreglo.');
+        return false;
+    }
+
+    const requiredEntityFieldProps = ["type", "name", "nameEntity", "isPrimaryKey", "isNotNull", "columnName"];
+    const missingEntityFieldProps = [];
+
+    config.entityFields.forEach((entityField, index) => {
+        requiredEntityFieldProps.forEach(prop => {
+            if (!entityField.hasOwnProperty(prop)) {
+                missingEntityFieldProps.push(`entityFields[${index}].${prop}`);
+            }
+        });
+    });
+
+    if (missingEntityFieldProps.length > 0) {
+        console.error(`Faltan los siguientes campos requeridos en los objetos de "entityFields": ${missingEntityFieldProps.join(', ')}`);
+        return false;
+    }
+
+    return true;
+}
 
 // Lee el archivo JSON
-fs.readFile('config.json', 'utf8', async (err, data) => {
+fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     if (err) {
         console.error('Error al leer el archivo:', err);
         return;
@@ -15,6 +64,12 @@ fs.readFile('config.json', 'utf8', async (err, data) => {
         config = JSON.parse(data);
     } catch (parseErr) {
         console.error('Error al parsear el archivo JSON:', parseErr);
+        return;
+    }
+
+    // Validar el esquema del JSON
+    if (!validateSchema(config)) {
+        console.error('El archivo JSON no cumple con el esquema esperado.');
         return;
     }
 
@@ -41,22 +96,14 @@ fs.readFile('config.json', 'utf8', async (err, data) => {
     // Llama a otros scripts según la configuración de las claves primarias
     if (primaryKeyCount === 1 && primaryKey.type === 'Long' && primaryKey.name === 'id') {
         console.log('Hay una única clave primaria del tipo Long con el nombre "id".');
-        executeScript('apiLong.js', 'config.json');
+        executeScript('apiLong.js', jsonFilePath);
     } else if (primaryKeyCount === 1 && (primaryKey.type !== 'Long' || primaryKey.name !== 'id')) {
         console.log('Hay una única clave primaria, pero no es del tipo Long o no se llama "id".');
-        executeScript('apiUnique.js', 'config.json');
+        executeScript('apiUnique.js', jsonFilePath);
     } else if (primaryKeyCount > 1) {
         console.log('Hay múltiples claves primarias.');
-        executeScript('apiDocker.js', 'config.json');
+        executeScript('apiDocker.js', jsonFilePath);
     } else {
         console.log('No hay claves primarias definidas.');
-    }
-
-    // Llama a setupRepo
-    try {
-        await setupRepo(config.projectName, config.username);
-        console.log('Repositorio configurado exitosamente.');
-    } catch (initRepoError) {
-        console.error('Error inicializando el repositorio:', initRepoError);
     }
 });
